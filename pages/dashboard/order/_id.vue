1323232b229e7be34b60d1493308af61b8086389
YusupovZA <jusupovz@gmail.com>
2021-02-22 09:40:09 +0500

    test

--- pages/dashboard/order/_id.vue
+++ pages/dashboard/order/_id.vue
@@ -1,5 +1,18 @@
 <template>
-  <div>order item</div>
+  <div class="px-11">
+    <div class="pt-3">
+      <nuxt-link to="/dashboard" class="ml-text-18-32-500">
+        <v-icon>mdi-chevron-left</v-icon>
+        Назад
+      </nuxt-link>
+    </div>
+    <div class="ml-text-13-18 my-8">Номер чека: 32349278348223</div>
+    <div>
+      <div class="ml-order-detail-position">
+        Велосипед Спортивный, двухколёсный, очень быстрый, с педалями
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
